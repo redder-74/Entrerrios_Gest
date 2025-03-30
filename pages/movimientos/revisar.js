c3941af7614cd5c3cc88ed1683892e1405901b3a
redder-74 <152974558+redder-74@users.noreply.github.com>
2025-03-30 21:19:36 +0200

    Add files via upload

---
+++ pages/movimientos/revisar.js
@@ -0,0 +1,11 @@
+// pages/movimientos/revisar.js
+import RevisarMovimientos from '../../components/RevisarMovimientos';
+import Layout from '../../components/Layout';
+
+export default function RevisarMovimientosPage() {
+  return (
+    <Layout>
+      <RevisarMovimientos />
+    </Layout>
+  );
+}
