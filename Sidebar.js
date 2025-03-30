1d8013c85ecf41fc6a7584586b5f578ae087f115
redder-74 <152974558+redder-74@users.noreply.github.com>
2025-03-30 11:36:26 +0200

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Sidebar() {
+  return (
+    <div className="w-64 bg-white shadow-md">
+      <div className="p-4 border-b">
+        <Image 
+          src="/public/logo.png" 
+          alt="Logo Entrerrios Dental" 
+          width={150} 
+          height={50} 
+        />
+      </div>
+      <nav className="p-4">
+        <ul className="space-y-2">
+          <li>
+            <Link href="/" className="block p-2 hover:bg-gray-100 rounded">
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link href="/movimientos/subir" className="block p-2 hover:bg-gray-100 rounded font-semibold">
+              Subir Movimientos
+            </Link>
+          </li>
+          {/* Añadir más opciones aquí luego */}
+        </ul>
+      </nav>
+    </div>
+  );
+}
